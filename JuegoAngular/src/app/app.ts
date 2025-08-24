e0d3926b9ce6c5f77a239c254447455df3ef44cf
Tepho <stephano.mejia@outlook.es>
2025-08-23 20:29:03 -0500

    Juego en Angular

--- JuegoAngular/src/app/app.ts
+++ JuegoAngular/src/app/app.ts
@@ -1,12 +1,65 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css']
 })
 export class App {
-  protected readonly title = signal('JuegoAngular');
+  title = signal('JuegoAngular');
+
+  turn = 1;
+
+  player1 = {
+    name: 'Drácula',
+    health: 100,
+    defend: false,
+    continue: false,
+  };
+
+  player2 = {
+    name: 'Frankenstein',
+    health: 100,
+    defend: false,
+    continue: false,
+  };
+
+  attack(player: number) {
+    if (player === 1) {
+      if (this.player2.defend) {
+        alert(`${this.player2.name} se ha defendido del ataque de ${this.player1.name}.`);
+        this.player2.defend = false;
+      } else {
+        this.player2.health -= 10;
+        alert(`${this.player1.name} ha atacado a ${this.player2.name}. Salud: ${this.player2.health}`);
+      }
+      this.turn = 2;
+    } else {
+      if (this.player1.defend) {
+        alert(`${this.player1.name} se ha defendido del ataque de ${this.player2.name}.`);
+        this.player1.defend = false;
+      } else {
+        this.player1.health -= 10;
+        alert(`${this.player2.name} ha atacado a ${this.player1.name}. Salud: ${this.player1.health}`);
+      }
+      this.turn = 1;
+    }
+  }
+
+  defend(player: number) {
+    if (player === 1 && !this.player1.continue) {
+      this.player1.defend = true;
+      this.player1.continue = true;
+      alert(`${this.player1.name} se ha defendido.`);
+      this.turn = 2;
+    } else if (player === 2 && !this.player2.continue) {
+      this.player2.defend = true;
+      this.player2.continue = true;
+      alert(`${this.player2.name} se ha defendido.`);
+      this.turn = 1;
+    } else {
+      alert("Ya usaste tu defensa.");
+    }
+  }
 }
